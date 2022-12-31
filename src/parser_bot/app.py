be62c4fcc600c67fb5821c2a5c78e11bf271ba25
1KUrii <egevh@bk.ru>
2022-12-31 19:05:46 +0300

    Syntax improvement, adding comments

--- src/parser_bot/app.py
+++ src/parser_bot/app.py
@@ -3,7 +3,7 @@ import asyncio
 import loguru
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher.filters import Text
-from tofunft_bot.src.parser_bot.price import get_last_price, parsing_floor_price, set_last_price
+from tofunft_bot.src.parser_bot.price import get_current_price, parse_lowest_price, set_current_price
 from data import TOKEN
 
 bot = Bot(token=TOKEN, parse_mode=types.ParseMode.HTML)
@@ -12,26 +12,25 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands='start')
 async def start(message: types.Message):
-    start_buttons = ['текущая цена', 'парсинг лучшей цены']
+    start_buttons = ['current price', 'parse lowest price']
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add(*start_buttons)
+    await message.answer('What you want ?', reply_markup=keyboard)
 
-    await message.answer('Чего желаете ?', reply_markup=keyboard)
 
-
-@dp.message_handler(Text(equals='текущая цена'))
+@dp.message_handler(Text(equals='current price'))
 async def get_lasted_price(message: types.Message):
-    await message.answer(f'Текущая цена : {await get_last_price()}')
+    await message.answer(f'current price : {await get_current_price()}')
 
 
-@dp.message_handler(Text(equals='парсинг лучшей цены'))
+@dp.message_handler(Text(equals='parse lowest price'))
 async def search_price(message: types.Message):
     while True:
         await asyncio.sleep(0.1)
-        low = await parsing_floor_price()
-        if low != await get_last_price():
-            await message.answer(f'Самая низкая цена : {low}')
-            await set_last_price(low)
+        low = await parse_lowest_price()
+        if low != await get_current_price():
+            await message.answer(f'parse lowest price : {low}')
+            await set_current_price(low)
 
 
 def main():
--- src/parser_bot/price.py
+++ src/parser_bot/price.py
@@ -5,37 +5,62 @@ from selenium.webdriver.chrome.options import Options
 from tofunft_bot.src.parser_bot.data import URL
 from tofunft_bot.src.parser_bot.data import DRIVER
 
-options = Options()
-options.add_argument('--disable-gpu')
-options.add_argument('--disable-blink-features=AutomationControlled')
-driver = webdriver.Chrome(
-    executable_path=DRIVER,
-    options=options)
-driver.get(URL)
-last_price = 0
+# Initialize Chrome webdriver with options to disable GPU and automation features
+chrome_options = Options()
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+webdriver = webdriver.Chrome(executable_path=DRIVER, options=chrome_options)
+
+# Load the webpage in the webdriver
+webdriver.get(URL)
+
+# Initialize variables
+current_price = 0
+lowest_price_found = float('inf')
 
 
-async def get_last_price():
-    return last_price
+async def get_current_price():
+    """Return the current price as a float."""
+    return current_price
 
 
-async def set_last_price(last):
-    global last_price
-    last_price = last
+async def set_current_price(new_price):
+    """Set the current price to the given value."""
+    global current_price
+    current_price = new_price
 
 
-async def parsing_floor_price():
-    global last_price
+async def parse_lowest_price():
+    """
+    Parse the lowest price from the webpage.
+    Return the lowest price as a float, or 'inf' if no price was found.
+    """
+    global lowest_price_found
 
     try:
-        page = driver.page_source  # raw html
-        soup = BeautifulSoup(page, 'html.parser')  # parsing html to text
-        lowest_price = soup.find_all('p', {'class': 'chakra-text css-0'})
-        best_price = float(lowest_price[0].text.replace(' ', '').replace('BNB', ''))
-        return best_price
+        webdriver.refresh()
 
-    except ValueError as ex:
+        # Get the raw HTML of the webpage
+        page_html = webdriver.page_source
+
+        # Parse the HTML using BeautifulSoup
+        soup = BeautifulSoup(page_html, 'html.parser')
+
+        # Find the element containing the lowest price
+        lowest_price_element = soup.find('p', {'class': 'chakra-text css-0'})
+
+        # Extract the numerical value from the element's text
+        if lowest_price_element:
+            lowest_price = float(lowest_price_element.text.replace(' ', '').replace('BNB', ''))
+        else:
+            lowest_price = float('inf')
+
+        # Update the lowest price found so far
+        lowest_price_found = min(lowest_price, lowest_price_found)
+
+        return lowest_price
+
+    except Exception as ex:
+        # Log any errors that occur
         loguru.logger.error(ex)
-
-    finally:
-        driver.refresh()
+        return float('inf')
